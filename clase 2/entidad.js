aab8c103f79e6dfea9801710c481eab1b5d0a111
saiegh@gmail.com <facundo.saiegh@luckyrabbitgames.com>
2024-08-19 18:01:09 -0300

    faltaba esto para q se entienda mejor

--- clase 2/entidad.js
+++ clase 2/entidad.js
@@ -1,26 +1,40 @@
 class Entidad {
   constructor(x, y, juego) {
     //CADA ENTIDAD GUARDA UNA REFERENCIA AL OBJETO DEL JUEGO
-    this.juego = juego;    
+    this.juego = juego;
     this.x = x;
     this.y = y;
+    this.velocidad = { x: 0, y: 0 };
+    this.acc={x:0,y:0}
     //LA ENTIDAD TIENE UNA PROPIEDAD Q ES EL GRAFICO DE PIXI
     this.grafico = new PIXI.Graphics()
       .rect(0, 0, 20, 20)
       .fill(getRandomColor());
 
-    
-
     //UNA VEZ Q SE CREA EL GRAFICO SE METE EN EL STAGE
     this.juego.pixiApp.stage.addChild(this.grafico);
   }
-  update(time) {
+  setearVelocidad(x, y) {
+    this.velocidad = { x: x, y: y };
+  }
+  setearFuerza(x,y){
+    this.acc.x = x;
+    this.acc.y = y;
+  }
+  update() {
     //FUNCION UPDATE = PENSAR COSAS
-    
+
     //MIRAR ALREDEDOR Y CONTEMPLAR LAS PROPIAS VARIABLES
     //SEGUN PROPIAS VARIABLES Y EL ENTORNO, CAMBIAR DE ESTADO
     //SEGUN ESTADO HACER COSAS
 
+    //AGREGO LA VELOCIDAD A LA POSICION
+    this.velocidad.x += this.acc.x
+    this.velocidad.y += this.acc.y
+    this.acc.x=0
+    this.acc.y=0
+    this.x += this.velocidad.x;
+    this.y += this.velocidad.y;
 
     //UNA VEZ LISTOS LOS CALCULOS, PODEMOS MOSTRAR COSAS:
     this.render();
@@ -30,7 +44,6 @@ class Entidad {
     //FUNCION RENDER=MOSTRAR COSAS
 
     //ACA SE PUEDE DECIDIR QUE SPRITE MOSTRAR
-
 
     //ACTUALIZAR POSICION
     this.grafico.x = this.x;
